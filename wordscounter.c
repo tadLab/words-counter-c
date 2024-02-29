636307f21126ac35d0b9a86d1e2f16d43670a28e
tadLab <161091442+tadLab@users.noreply.github.com>
2024-02-29 19:58:58 +0100

    +test

--- wordscounter.c
+++ wordscounter.c
@@ -3,11 +3,23 @@
 #include <stdbool.h>
 #include <ctype.h>
 
+// CHECKS IF THE USER INPUT SOMETHING
+bool isNull(const char *string){
+   
+    for (int i = 0; string[i] != '\0'; i++) {
+        if (!isspace(string[i])) {
+            return false; 
+        }
+    }
+
+    return true; 
+}
+
 //CHECKS IF THERE ARE ONLY WORDS ˆNUMBERS, SYMBOLS
 bool isString(const char string[1001]) {
 
-    for (int index = 0; index < strlen(string); index++) {
-        if (!isalpha(string[index]) && !isspace(string[index])) {
+    for(int index = 0; index < strlen(string); index++) {
+        if(!isalpha(string[index]) && !isspace(string[index])) {
             return false;
         }
     }
@@ -40,11 +52,14 @@ int main(int argc, char const *argv[])
     printf("Enter your text : ");
     fgets(userInput, sizeof(userInput), stdin);
 
-
-    if(!isString(userInput)){
-        printf("There are symbols or numbers in the input.\n");
+    if(!isNull(userInput)){
+       if(!isString(userInput)){
+            printf("There are symbols or numbers in the input.\n");
+        }else{
+            printf("Number of words : %d\n", counter(userInput));
+        }
     } else{
-        printf("Number of words : %d\n", counter(userInput));
+        printf("Please provide some input.\n");
     }
 
     return 0;
